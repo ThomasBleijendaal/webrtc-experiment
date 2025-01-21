7c3259b31d5c6be5de36f816d4bc5ecfca88b6bd
Thomas Bleijendaal <t.bleijendaal@wearetriple.com>
2025-01-21 21:45:51 +0100

    Working signaling server + multi-peer connections

--- GameHost/GameHost/wwwroot/main.js
+++ GameHost/GameHost/wwwroot/main.js
@@ -1,141 +1,132 @@
-function createMessage(type, content) { return JSON.stringify({ type: type, content: content }); };
+function createMessage(type, from, to, content) { return JSON.stringify({ type: type, from: from, to: to, content: content }); };
 
-
+const target = document.querySelector("#messages");
 
 class RtcsManager {
-    static ws = new WebSocket(`ws://${location.host}/ws`);
+    static ws = null;
 
-    static pc = new RTCPeerConnection({
-        iceServers: []
-    });
+    static id = `U${Date.now()}${Math.random()}`;
+
+    static peers = {};
 
     static dataChannel;
 
     static {
+        this.ws = new WebSocket(`ws://${location.host}/ws`);
+
         this.ws.onmessage = (event) => {
             let message = JSON.parse(event.data);
 
+            let from = message.from;
+            let to = message.to;
+
             console.log("WS", message);
+            if (to != null && to != this.id) {
+                console.log("IGNORING");
+                return;
+            }
 
             if (message.type === "join") {
 
-                let id = message.content.id;
+                let newPeer = new Rtc(this.ws, from, this.id);
 
+                newPeer.createOffer();
 
+                this.peers[from] = newPeer;
 
-            } else if (message.type === "offer" || message.type === "answer") {
+                console.log("JOIN", from);
 
+            } else if (message.type === "offer") {
+
+                if (!this.peers[from]) {
+                    console.log("OFFER", from);
+                    this.peers[from] = new Rtc(this.ws, from, this.id);
+                    this.peers[from].handleRemote(message.type, message.content);
+                }
+                else {
+                    console.log("OFFER ALREADY KNOWN");
+                }
+
+            } else if (message.type === "answer") {
+
+                if (this.peers[from]) {
+                    console.log("ANWER", from);
+
+                    if (this.peers[from].isConnected()) {
+                        console.log("ALREADY KNOWN", from);
+                    }
+                    else {
+                        this.peers[from].handleRemote(message.type, message.content);
+                    }
+                }
                 
-
-                document.querySelector("#remoteSdp").value = message.content;
-                this.createAnswer(message.type, message.content);
-
             } else if (message.type === "candidate") {
 
                 let candidate = message.content;
 
                 if (candidate) {
-                    document.querySelector("#remoteCandidate").value = JSON.stringify(candidate);
-                    this.pc.addIceCandidate(new RTCIceCandidate(candidate));
+                    if (this.peers[from]) {
+                        console.log("CANDIDATE", from);
+                        this.peers[from].addCandicate(message.content);
+                    }
                 }
             }
         };
 
-        this.ws.send(createMessage("join", { id: `U${performance.now().toString()}${Math.random()}` }));
+        this.ws.onopen = (event) => {
+            console.log("WS open");
 
-        this.pc.onicecandidate = (event) => {
-            console.log("ICE", event);
-            if (event.type === "icecandidate" && event.candidate) {
-
-                this.ws.send(createMessage("candidate", event.candidate));
-
-                if (event.candidate.foundation === "0") {
-                    document.querySelector("#localCandidate").value = JSON.stringify(event.candidate);
-                }
-            }
-        };
-
-        this.pc.ondatachannel = (event) => {
-            console.log("DATACHANNEL", event);
-
-            this.dataChannel = event.channel;
-
-            this.dataChannel.onopen = (event) => {
-                console.log("DATA", event);
-            };
-
-            this.dataChannel.onmessage = (event) => {
-                console.log("MESSAGE", event);
-            };
-        };
-    }
-
-    static createOffer = function () {
-        this.dataChannel = this.pc.createDataChannel('main-channel');
-
-        this.dataChannel.onopen = (event) => {
-            console.log("DATA", event);
-        };
-
-        this.dataChannel.onmessage = (event) => {
-            console.log("MESSAGE", event);
-        };
-
-        this.pc.createOffer({
-            offerToReceiveAudio: false,
-            offerToReceiveVideo: false
-        }).then(offer => {
-            this.pc.setLocalDescription(offer);
-
-            document.querySelector("#localSdp").value = offer.sdp;
-
-            this.ws.send(createMessage("offer", offer.sdp));
-        });
-    }
-
-    static createAnswer = function (type, sdp) {
-        this.pc.setRemoteDescription({
-            type: type,
-            sdp: sdp
-        });
-
-        if (type === "offer") {
-            this.pc.createAnswer().then(answer => {
-                this.pc.setLocalDescription(answer);
-
-                document.querySelector("#localSdp").value = answer.sdp;
-
-                this.ws.send(createMessage("answer", answer.sdp));
-            });
+            this.ws.send(createMessage("join", this.id, null, { id: this.id }));
         }
     }
 
+    static remove = function (peerId) {
+        this.peers[peerId] = null;
+    }
+
     static sendStuff = function () {
-        this.dataChannel.send("HI");
+        for (let peer of Object.values(this.peers)) {
+            console.log("SENDING TO", peer.id);
+            peer.sendStuff();
+        }
     }
 }
 
 class Rtc {
-    pc = new RTCPeerConnection({
-        iceServers: []
-    });
+    remote = null;
+    local = null;
+
+    pc = null;
 
     dataChannel = null;
 
     ws = null;
 
-    constructor(ws) {
+    msgCount = 0;
+
+    constructor(ws, remote, local) {
+        this.remote = remote;
+        this.local = local;
+
+        this.pc = new RTCPeerConnection({
+            iceServers: []
+        });
+
         this.ws = ws;
 
         this.pc.onicecandidate = (event) => {
             console.log("ICE", event);
             if (event.type === "icecandidate" && event.candidate) {
+                this.ws.send(createMessage("candidate", this.local, this.remote, event.candidate));
+            }
+        };
 
-                this.ws.send(createMessage("candidate", event.candidate));
+        this.pc.onconnectionstatechange = (event) => {
+            console.log("CONNECTION CHANGE", event);
 
-                if (event.candidate.foundation === "0") {
-                    document.querySelector("#localCandidate").value = JSON.stringify(event.candidate);
-                }
+            if (this.pc.connectionState == "closed" || this.pc.connectionState == "failed") {
+                console.log("REMOVE", this.pc.remote);
+                RtcsManager.remove(this.pc.remote);
             }
         };
 
@@ -143,27 +134,13 @@ class Rtc {
             console.log("DATACHANNEL", event);
 
             this.dataChannel = event.channel;
-
-            this.dataChannel.onopen = (event) => {
-                console.log("DATA", event);
-            };
-
-            this.dataChannel.onmessage = (event) => {
-                console.log("MESSAGE", event);
-            };
+            this.setupChannel();
         };
     }
 
     createOffer = function () {
         this.dataChannel = this.pc.createDataChannel('main-channel');
-
-        this.dataChannel.onopen = (event) => {
-            console.log("DATA", event);
-        };
-
-        this.dataChannel.onmessage = (event) => {
-            console.log("MESSAGE", event);
-        };
+        this.setupChannel();
 
         this.pc.createOffer({
             offerToReceiveAudio: false,
@@ -171,13 +148,16 @@ class Rtc {
         }).then(offer => {
             this.pc.setLocalDescription(offer);
 
-            document.querySelector("#localSdp").value = offer.sdp;
-
-            this.ws.send(createMessage("offer", offer.sdp));
+            this.ws.send(createMessage("offer", this.local, this.remote, offer.sdp));
         });
     }
 
-    createAnswer = function (type, sdp) {
+    isConnected = function () {
+        console.log(this.pc.signalingState);
+        return this.pc.signalingState === "stable";
+    }
+
+    handleRemote = function (type, sdp) {
         this.pc.setRemoteDescription({
             type: type,
             sdp: sdp
@@ -187,10 +167,30 @@ class Rtc {
             this.pc.createAnswer().then(answer => {
                 this.pc.setLocalDescription(answer);
 
-                document.querySelector("#localSdp").value = answer.sdp;
-
-                this.ws.send(createMessage("answer", answer.sdp));
+                this.ws.send(createMessage("answer", this.local, this.remote, answer.sdp));
             });
+        }
+    }
+
+    setupChannel = function () {
+        this.dataChannel.onopen = (event) => {
+            console.log("DATA", event);
+        };
+
+        this.dataChannel.onmessage = (event) => {
+            console.log("MESSAGE", event);
+
+            this.msgCount++;
+
+            console.log(this.msgCount);
+
+            target.value = target.value + event.data;
+        };
+    }
+
+    addCandicate = function (candidate) {
+        if (candidate) {
+            this.pc.addIceCandidate(new RTCIceCandidate(candidate));
         }
     }
 
@@ -199,12 +199,5 @@ class Rtc {
     }
 }
 
-document.querySelector("#createOffer").onclick = function () { Rtc.createOffer(); };
-document.querySelector("#createAnswer").onclick = function () { Rtc.createAnswer(true); };
-document.querySelector("#setAnswer").onclick = function () { Rtc.createAnswer(false); };
-document.querySelector("#sendStuff").onclick = function () { Rtc.sendStuff(); };
-
-document.querySelector("#localSdp").value = "";
-document.querySelector("#remoteSdp").value = "";
-document.querySelector("#localCandidate").value = "";
-document.querySelector("#remoteCandidate").value = "";
+document.querySelector("#sendStuff").onclick = function () { RtcsManager.sendStuff(); };
+document.querySelector("#messages").value = "";
